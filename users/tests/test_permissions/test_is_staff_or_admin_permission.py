d00689b38d35a41ed2fdaccd1f19370d4455149b
Tzintzun444 <alfredotzintzun4@gmail.com>
2025-07-02 09:45:46 -0600

    Tests for IsStaffOrAdminPermission were added

---
+++ users/tests/test_permissions/test_is_staff_or_admin_permission.py
@@ -0,0 +1,25 @@
+from users.permissions import IsStaffOrAdmin
+from rest_framework.test import APIRequestFactory
+import pytest
+
+factory = APIRequestFactory()
+
+
+@pytest.mark.parametrize('is_staff,is_superuser,expected', [
+    (True, True, True),
+    (True, False, True),
+    (False, True, True),
+    (False, False, False)
+])
+@pytest.mark.django_db
+def test_is_staff_or_admin_permission(general_user, is_staff, is_superuser, expected):
+
+    general_user.is_staff = is_staff
+    general_user.is_superuser = is_superuser
+    general_user.save()
+    request = factory.get('/fake-url/')
+    request.user = general_user
+    mock_view = object()
+    permission = IsStaffOrAdmin()
+
+    assert permission.has_permission(request, mock_view) is expected
